8cfefe51f89cd7637b3cf3bd9abf06d1df220990
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-07-22 22:40:11 -0400

    Store place name and location in firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,21 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+
+    fetch('https://awesome-places-43811.firebaseio.com/places.json', {
+      method: 'POST',
+      body: JSON.stringify(placeData)
+    })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      })
+      .catch(err => console.log(err));
   };
 };
 
