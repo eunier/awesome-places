f4036d92b837b747b52c55aad854ec764ca3c3c5
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-07-27 15:02:34 -0400

    Store token in AsyncStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import { AsyncStorage } from 'react-native';
+
 import { TRY_AUTH, AUTH_SET_TOKEN } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from './index';
 import startMainTabs from '../../screens/MainTabs/startMainTabs';
@@ -37,7 +39,7 @@ export const tryAuth = (authData, authMode) => {
         if (!parsedRes.idToken) {
           alert('Authentication failed, please try again!');
         } else {
-          dispatch(authSetToken(parsedRes.idToken));
+          dispatch(authStoreToken(parsedRes.idToken));
           startMainTabs();
         }
       });
@@ -51,13 +53,25 @@ export const authSetToken = token => {
   };
 };
 
+export const authStoreToken = token => {
+  return dispatch => {
+    dispatch(authSetToken(token));
+    AsyncStorage.setItem('ap:auth:token', token);
+  };
+};
+
 export const authGetToken = () => {
   return (dispatch, getState) => {
     const promise = new Promise((resolve, reject) => {
       const token = getState().auth.token;
 
       if (!token) {
-        reject();
+        AsyncStorage.getItem('ap:auth:token')
+          .catch(err => reject())
+          .then(tokenFromStorage => {
+            dispatch(authSetToken(tokenFromStorage));
+            resolve(tokenFromStorage);
+          });
       } else {
         resolve(token);
       }
